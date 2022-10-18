c64d16a142124e0a329a140637dcab47273e0938
Leonardo Fagner Pires Soares <leonardofps@gmail.com>
2022-10-18 15:52:07 -0300

    Não salva vídeos na pasta

--- cypress.config.js
+++ cypress.config.js
@@ -3,8 +3,10 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     video: false,
+    // Não salva vídeos na pasta
     setupNodeEvents(on, config) {
       // implement node event listeners here
+
 
      
      
